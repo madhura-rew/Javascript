dd5ec47d07a3dfc0e2138566e1b73cae2a1cfd21
madhura-rew <madhurarewatkar1076@gmail.com>
2026-07-07 14:07:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+// This file is linked in the <head> of the HTML
+console.log("External JavaScript file loaded successfully.");
+
+// You can add extra functions here that are called from the HTML
+function externalGreeting() {
+    return "Hello from the external file!";
+}
