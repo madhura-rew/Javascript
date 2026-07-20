c088fa46b7b116d320ee4f7fcc8b1031c4c616c9
madhura-rew <madhurarewatkar1076@gmail.com>
2026-07-20 20:41:47 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// script.js
-// This file is linked in the <head> of the HTML
-console.log("External JavaScript file loaded successfully.");
-
-// You can add extra functions here that are called from the HTML
-function externalGreeting() {
-    return "Hello from the external file!";
-}
